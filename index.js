13922a5b91cc7c7f841c73d7f952d4e3449968d2
Arthur O. Odwor <aodwor@usiu.ac.ke>
2022-03-10 12:59:13 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// var http = require("http")
+// var port = 3000
+
+// var requestHandler = function(request, response){
+//     console.log(request.url);
+//     response.end('Hello from node server');
+// }
+
+// var server = http.createServer(requestHandler);
+// server.listen(port);
+
+// console.log("node server stared on port 3000")
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const bodyParser = require('body-parser');
+// const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+// app.set('views', 'views');
+// app.set('view engine', 'hbs');
+// app.use(express.static('public'));
+
+// app.get('/contacts', function(request, response){
+//     response.render('contact_us')
+// })
+
+// app.post('/process-contacts', urlEncodedParser, function(request, response){
+//     response.end('Thank you ' + request.body.first_name + " " + request.body.last_name);
+// });
+
+
+// app.listen(port);
+// console.log('server is listening on port 3000')
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+app.set('views', 'views');
+app.set('view engine', 'hbs');
+app.use(express.static('public'));
+
+app.get('/contacts', function(request, response){
+    response.render('contact_us')
+})
+
+app.post('/process-contacts', urlEncodedParser, function(request, response){
+    var weight = request.body.weight;
+    var height = request.body.height;
+    var bmi = weight / (height * height);
+    response.end('Thank you for submitting, your bmi is: ' + bmi);
+});
+
+
+app.listen(port);
+console.log('server is listening on port 3000')
+
